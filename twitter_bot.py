fc136e0ee67f6ec1d083a2fcd7378687edec7636
Kasahara Seiji <seiji-k@tomato.naist.jp>
2010-06-04 10:58:35 +0900

    edit mail

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
-#Author: Seiji-k<seiji.naist@gmail.com>
+#Author: Seiji-k<seijik42@gmail.com>
 
 # Using python-twitter; a python wrapper around the Twitter API
 # (http://code.google.com/p/python-twitter/)
